362e828068862755870f098cf412ea2063d43213
sabyryou <sakhibarraq12@gmail.com>
2025-07-02 02:19:21 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,19 @@
+function kirim(e) {
+    e.preventDefault();
+    const input = document.getElementById("nama").value;
+    alert("Halo " + input +", i see you");
+  }
+  
+
+  function umur(mantap){
+    const input = document.getElementById("tanggal lahir").value;
+    alert("umur kamu " + input); 
+  }
+
+
+  function lifeupdate(batagor){
+    const input = document.getElementById("story").value;
+    alert(input);
+
+  }
+  
